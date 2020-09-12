34eca1736a1dd35d4ad0c9836d1dc71e5f32256d
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-09-12 13:06:02 -0400

    fetch api and city search function

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,26 @@
-fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=43fd1cdd770e5cf56daf2f9d5cdc1037')
-.then(response => response.json())
-.then(data => console.log(data));
+// fetch('http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=43fd1cdd770e5cf56daf2f9d5cdc1037')
+// .then(response => response.json())
+// .then(data => console.log(data));
+
+// fetch('api.openweathermap.org/data/2.5/weather?q={city name}&appid=524901&APPID=43fd1cdd770e5cf56daf2f9d5cdc1037')
+// .then(response => response.json())
+// .then(data => console.log(data));
+
+var citySearch = function (city) {
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=43fd1cdd770e5cf56daf2f9d5cdc1037";
+    console.log(apiUrl);
+
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+            
+        })
+       
+    });
+    
+};
+
+citySearch('Tampa');
+
+
+// http://api.openweathermap.org/data/2.5/weather?q=London&appid=43fd1cdd770e5cf56daf2f9d5cdc1037

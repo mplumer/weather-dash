9228152396ba66b5570b118eb49841bcb49a1557
Max Plumer <68719068+mplumer@users.noreply.github.com>
2020-09-12 13:26:37 -0400

    add weather img, display date, convert units

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,11 +5,25 @@
 // fetch('api.openweathermap.org/data/2.5/weather?q={city name}&appid=524901&APPID=43fd1cdd770e5cf56daf2f9d5cdc1037')
 // .then(response => response.json())
 // .then(data => console.log(data));
+
+// DISPLAY CURRENT DATE IN HEADER
+// var showTime = function () {
+//     $("#year").text(moment().format('YYYY'));
+//     $("#date").text(moment().format('dddd, MM/DD/YYYY'));
+//     $("#time").text(moment().format('hh:mm a'));
+// }
+// setInterval(showTime, 1000); 
+
 var searchFormEl = document.querySelector("#form-input");
 var cityInputEl = document.querySelector("#searchTerm");
 var cityDisplayName = document.querySelector("#city");
-var tempEl = document.querySelector("#temp");
-var humidityContainer = document.querySelector("#humidity");
+var iconEl = document.querySelector("#icon");
+var temp = document.querySelector("#temp");
+var humidity = document.querySelector("#humidity");
+var windSpeed = document.querySelector("#wind");
+var uvIndex = document.querySelector("#uv");
+var currentDate = document.querySelector("#date");
+var lat = "lat";
 
 var formSubmitHandler = function (event) {
     event.preventDefault();
@@ -26,12 +40,13 @@ var formSubmitHandler = function (event) {
 };
 
 var citySearch = function (city) {
-    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=43fd1cdd770e5cf56daf2f9d5cdc1037";
+    var apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=43fd1cdd770e5cf56daf2f9d5cdc1037";
     console.log(apiUrl);
 
     fetch(apiUrl).then(function (response) {
         response.json().then(function (data) {
             displayWeather(data, city);
+
 
         })
 
@@ -41,23 +56,44 @@ var citySearch = function (city) {
 
 var displayWeather = function (data, city) {
     cityDisplayName.textContent = city;
-    tempEl.textContent = "Temperature:";
+    iconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+    temp.textContent = "Temperature: " + data.main.temp;
+    humidity.textContent = "Humidity: " + data.main.humidity;
+    windSpeed.textContent = "Wind Speed: " + data.wind.speed;
 
-    // for (var i = 0; i < data.length; i++) {
+    // Convert UTC code to current date
+    var milliseconds = data.dt * 1000;
+    var dateObject = new Date(milliseconds);
+    var dateToday = dateObject.toLocaleDateString
+        ('en-US');
+    currentDate.textContent = dateToday
 
-    // }
-    var humidityEL = document.createElement("span");
-    humidityEL.textContent = " " + data.main.humidity;
-    humidityContainer.appendChild(humidityEL);
+    var lat = data.coord.lat
+    var lon = data.coord.lon
+    var uvUrl = "http://api.openweathermap.org/data/2.5/uvi?lat=" + lat + "&lon=" + lon + "&appid=43fd1cdd770e5cf56daf2f9d5cdc1037";
+    uvIndex.textContent = "UV Index: " + lat + lon;
+
+    fetch(uvUrl).then(function (response) {
+        response.json().then(function (data) {
+            displayUV(data);
+        })
+    })
 
     console.log(data);
-    console.log(city);
-    console.log(data.main.humidity);
-
 };
 
+var displayUV = function (data) {
+    uvIndex.textContent = "UV Index: " + data.value
+    console.log(data);
+}
+
 searchFormEl.addEventListener("submit", formSubmitHandler);
+// showTime()
 
 
 
 // http://api.openweathermap.org/data/2.5/weather?q=London&appid=43fd1cdd770e5cf56daf2f9d5cdc1037
+
+// http://openweathermap.org/img/wn/" + value + "@2x.png
+
+// "http://api.openweathermap.org/data/2.5/uvi?lat=30.27&lon=-97.74&appid=43fd1cdd770e5cf56daf2f9d5cdc1037"
